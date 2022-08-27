887f1458cd1183977f6db2a8bf68f1bb3cff5a84
fragler01 <60200282+fragler01@users.noreply.github.com>
2022-08-27 17:52:51 +0200

    fel-le-nyilak
    Hozzáadtam a taskokhoz a fel-le nyilakat és működésre is bírtam őket. Egyelőre még látszanak akkor is, ha nem lehet az adott elemet fel vagy le mozgatni, de nem működnek hibásan. Ezen felelül beállítottam, hogy amikor a plusz gombra nyomunk, akkor a következő adat felvitelekor ne kelljen külön belekattintani a mezőbe, hanem megcsinálja magától

--- to-do-list/app.js
+++ to-do-list/app.js
@@ -1,6 +1,7 @@
 let addButton = document.getElementById("add");
 let inputField = document.getElementById("input");
 let tasks = document.getElementById("tasks");
+let tasksArray = [];
 
 
 
@@ -12,8 +13,11 @@ inputField.addEventListener('keyup', function (e) {
 });
 
 addButton.addEventListener('click', add);
+addButton.addEventListener('hover', function(){
 
-function add() {
+})
+
+function add() {    
     if (inputField.value != "") {
         let newItem = document.createElement('div');
         newItem.classList.add("task");
@@ -28,14 +32,16 @@ function add() {
         let tools = document.createElement('div');
         tools.classList.add("tools");
 
+        //kuka ikon és működése
         let trashIcon = document.createElement('i');
         trashIcon.classList.add("fa-solid");
         trashIcon.classList.add("fa-trash");
         trashIcon.classList.add("tool");
-        trashIcon.addEventListener('click', function(){
-            newItem.remove();
+        trashIcon.addEventListener('click', function(){            
+            newItem.remove();        
         })
 
+        //ceruza ikon és működése
         let pencilIcon = document.createElement('i');
         pencilIcon.classList.add("fa-solid");
         pencilIcon.classList.add("fa-pencil");
@@ -52,22 +58,56 @@ function add() {
                     p.innerText = editText.value;
                     p.hidden = false;
                     editText.remove();
-                    newItem.hidden = false;    
-                                    
+                    newItem.hidden = false;                                        
                 }
             })
+            newItem.classList.toggle("done");
         })
 
+        //lefele nyíl és működése
+        let downIcon = document.createElement('i');
+        downIcon.classList.add("fa-solid");
+        downIcon.classList.add("fa-arrow-down");
+        downIcon.classList.add("tool");
+        downIcon.addEventListener('click', function(){      
+            newItem.classList.toggle("done");
+            let current = this.parentElement.parentElement;
+            let next = current.nextElementSibling;
+            if(next != null) {
+                next.after(current);    
+            }                        
+        })
+        
+        //felfele nyíl és működése
+        let upIcon = document.createElement('i');
+        upIcon.classList.add("fa-solid");
+        upIcon.classList.add("fa-arrow-up");
+        upIcon.classList.add("tool");
+        upIcon.addEventListener('click', function(){      
+            newItem.classList.toggle("done");
+            let current = this.parentElement.parentElement;
+            let previous = current.previousElementSibling;
+            if(previous != null) {
+                previous.before(current);    
+            }                        
+        })
 
         tools.prepend(trashIcon);
         tools.append(pencilIcon);
+        tools.append(downIcon);
+        tools.append(upIcon);
 
         newItem.append(tools);
         newItem.prepend(p);
-        tasks.prepend(newItem);
+        tasks.append(newItem);
+        tasksArray.push(newItem);
 
-        inputField.value = "";
+        inputField.value = "";      
+        console.log(tasksArray);
+        console.log(tasks);    
+        console.log(newItem.nextElementSibling);          
     }
+    inputField.focus();
 }
 
 
