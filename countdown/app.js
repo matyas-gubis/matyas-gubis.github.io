a957a47f5513d81d076684705385009e0d740ef0
fragler01 <60200282+fragler01@users.noreply.github.com>
2022-09-04 14:32:14 +0200

    Update app.js

--- countdown/app.js
+++ countdown/app.js
@@ -3,7 +3,7 @@ let result = document.querySelector('h1');
 showTime();
 function showTime(){
     let today = new Date();
-    let andras = new Date(2022, 09, 08, 22, 15, 0, 0);
+    let andras = new Date(2022, 09, 08, 22, 15, 0, 0);    
     let eredmeny = andras.getTime() - today.getTime(); 
     let datum = new Date(eredmeny)      
     result.innerHTML = `${datum.getDate()}&nbspnap ${datum.getHours()-1}&nbspóra ${datum.getMinutes()}&nbspperc és ${datum.getSeconds()}&nbspmp`;
