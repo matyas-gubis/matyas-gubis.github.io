a2ac9980d12e8dbbf0307d1d2a34e597903c059b
fragler01 <60200282+fragler01@users.noreply.github.com>
2022-09-04 14:26:13 +0200

    Andras-timer

---
+++ countdown/app.js
@@ -0,0 +1,13 @@
+let result = document.querySelector('h1');
+
+showTime();
+function showTime(){
+    let today = new Date();
+    let andras = new Date(2022, 09, 08, 22, 15, 0, 0);
+    let eredmeny = andras.getTime() - today.getTime(); 
+    let datum = new Date(eredmeny)      
+    result.innerHTML = `${datum.getDate()}&nbspnap ${datum.getHours()}&nbspóra ${datum.getMinutes()}&nbspperc és ${datum.getSeconds()}&nbspmp`;
+
+    setInterval(showTime, 1000);
+
+}
